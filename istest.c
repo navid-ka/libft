344d73d261ab32ea07ca2bc182042bdc643a8eaa
navid-ka <psychodelictrip@gmail.com>
2023-04-23 21:50:00 +0200

    atoi test ok

--- istest.c
+++ istest.c
@@ -8,17 +8,17 @@
 void ft_ischeck(int c, int(*f)(int), int(*f2)(int), char *name, char *name2)
 {
 	if (f(c) == f2(c))
-		printf("\033[0;32mTest passed of %s is equal to %s [√]\n\033[m", name, name2);
+		printf("\033[0;36m<<TEST CHECKING... \n\033[0;32mTest passed of %s is equal to %s [√]\n\033[m", name, name2);
 	else
-		printf("\033[0;31mTest of %s failed [X]\n\033[m", name);
+		printf("\033[0;36m<<TEST CHECKING... \n\033[0;31mTest of %s failed [X]\n\033[m", name);
 }
 
-void ft_ischeckstr(char *c, int(*f)(char *), int(*f2)(char *), char *name, char *name2)
+void ft_ischeckstr(const char *str, int(*f)(const char *), int(*f2)(const char *), char *name, char *name2)
 {
-	if (f(c) == f2(c))
-		printf("\033[0;32mTest passed of %s is equal to %s [√]\n\033[m", name, name2);
-	else
-		printf("\033[0;31mTest of %s failed [X]\n\033[m", name);
+    if (f(str) == f2(str))
+        printf("\033[0;36m<<TEST CHECKING... \n\033[0;32mTest passed of %s is equal to %s [√]\n\033[m", name, name2);
+    else
+        printf("\033[0;36m<<TEST CHECKING... \n\033[0;31mTest of %s failed [X]\n\033[m", name);
 }
 
 int main(void)
@@ -32,39 +32,27 @@ int main(void)
 	testn = '9';
 	testc = 'a';
 
-	printf("\033[0;36m<<TEST CHECKING... \n");
 	// ISALPHA returns non-zero value if true mine returns true isalpha returns 1024
 	ft_ischeck(testc, &ft_isalpha, &isalpha, "ft_isalpha", "isalpha");
-	printf("\033[0;36m<<TEST CHECKING... \n");
 	//ISDIGIT
 	ft_ischeck(testnb, &ft_isdigit, &isdigit,  "ft_isdigit", "isdigit");
-	printf("\033[0;36m<<TEST CHECKING... \n");
 	//ISASCII
 	ft_ischeck(testn, &ft_isascii, &isascii, "ft_isascii", "isascii");
-	printf("\033[0;36m<<TEST CHECKING... \n");
 	//ISALNUM
 	ft_ischeck(testnb, &ft_isalnum, &isalnum, "ft_isalnum", "isalnum");
-	printf("\033[0;36m<<TEST CHECKING... \n");
 	//ISPRINT returns non-zero value if true mine returns 1 the isprint number 16384
 	ft_ischeck(testc, &ft_isprint, &isprint, "ft_isprint", "isprint");
-	printf("\033[0;36m<<TEST CHECKING... \n");
 	// TOUPPER
 	ft_ischeck(testc, &ft_toupper, &toupper, "ft_toupper", "toupper");
-	printf("\033[0;36m<<TEST CHECKING... \n");
 	// TOLOWER
 	ft_ischeck(testc, &ft_tolower, &tolower, "ft_tolower", "tolower");
 
-	//STRLEN
-	//ft_ischeckstr(testd, &ft_strlen, &strlen, "ft_strlen", "strlen");
+
 	//ATOI
-	//ft_ischeckstr((char *)testd, &ft_atoi, &atoi, "ft_atoi", "atoi");
-	char str[10] = {'1','2','1','5','6'};
-	int moi;
-	int moi2;
-	moi = atoi(str);
-	printf("%d \n",moi);
-	moi2 = ft_atoi(str);
-	printf("%d \n",moi2);
+	char str[] = "12345";
+    ft_ischeckstr(str, &ft_atoi, &atoi, "ft_atoi", "atoi");
+	//STRLEN WIP look what is t_size
+   	//ft_ischeckstr(str, &ft_strlen, &strlen, "ft_strlen", "strlen");
 
 	return (0);
 }
