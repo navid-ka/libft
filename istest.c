1bc6c9ae31df7b3f002d921d41d3237f84b13e56
navid-ka <psychodelictrip@gmail.com>
2023-04-23 21:24:40 +0200

    makefile and testings

--- istest.c
+++ istest.c
@@ -2,13 +2,23 @@
 #include <ctype.h>
 #include <unistd.h>
 #include <libft.h>
+#include <stdlib.h>
+#include <string.h>
 
 void ft_ischeck(int c, int(*f)(int), int(*f2)(int), char *name, char *name2)
 {
 	if (f(c) == f2(c))
 		printf("\033[0;32mTest passed of %s is equal to %s [√]\n\033[m", name, name2);
 	else
-		printf("\033[0;31mTest of %s returned [X]\n\033[m", name);
+		printf("\033[0;31mTest of %s failed [X]\n\033[m", name);
+}
+
+void ft_ischeckstr(char *c, int(*f)(char *), int(*f2)(char *), char *name, char *name2)
+{
+	if (f(c) == f2(c))
+		printf("\033[0;32mTest passed of %s is equal to %s [√]\n\033[m", name, name2);
+	else
+		printf("\033[0;31mTest of %s failed [X]\n\033[m", name);
 }
 
 int main(void)
@@ -16,10 +26,12 @@ int main(void)
 	int testnb;
 	int testn;
 	char testc;
+	//const char testd[200] = "9";
 
 	testnb = 9;
 	testn = '9';
 	testc = 'a';
+
 	printf("\033[0;36m<<TEST CHECKING... \n");
 	// ISALPHA returns non-zero value if true mine returns true isalpha returns 1024
 	ft_ischeck(testc, &ft_isalpha, &isalpha, "ft_isalpha", "isalpha");
@@ -42,6 +54,17 @@ int main(void)
 	// TOLOWER
 	ft_ischeck(testc, &ft_tolower, &tolower, "ft_tolower", "tolower");
 
+	//STRLEN
+	//ft_ischeckstr(testd, &ft_strlen, &strlen, "ft_strlen", "strlen");
+	//ATOI
+	//ft_ischeckstr((char *)testd, &ft_atoi, &atoi, "ft_atoi", "atoi");
+	char str[10] = {'1','2','1','5','6'};
+	int moi;
+	int moi2;
+	moi = atoi(str);
+	printf("%d \n",moi);
+	moi2 = ft_atoi(str);
+	printf("%d \n",moi2);
 
 	return (0);
 }
