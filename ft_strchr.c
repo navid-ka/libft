67c114be4e92731c28192e7ae66dd5dbbc52dd92
Tu Nombre <you@example.com>
2023-05-02 23:41:08 +0200

    strchr perfect paco aproved

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,10 +14,16 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s++)
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
 	{
-		if (c == *s)
-			return ((char *)s);
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
 	}
+	if ((char)c == s[i])
+		return ((char *)&s[i]);
 	return (NULL);
 }
