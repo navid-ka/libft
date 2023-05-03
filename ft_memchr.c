897448d6a48ceadc3acd3e01764357a6bc82d9ed
Bifrost <nkeyani-@student.42barcelona.com>
2023-05-03 12:15:35 +0200

    memchr paco ok

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,24 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/03 11:42:01 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/03 12:08:58 by nkeyani-         ###   ########.fr       */
+/*   Created: 2023/05/03 12:03:25 by nkeyani-          #+#    #+#             */
+/*   Updated: 2023/05/03 12:14:40 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*ptrs;
 	size_t			i;
 
-	ptrs = (unsigned char *)s;
 	i = 0;
+	ptrs = (unsigned char *)s;
 	while (i < n)
-		ptrs[i++] = 0;
+	{
+		if (ptrs[i] == (unsigned char)c)
+			return (&ptrs[i]);
+		i++;
+	}
+	return (NULL);
 }
