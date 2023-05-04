7f0b8287eb41a95ba3806b8a9a71e36ca2575015
Tu Nombre <you@example.com>
2023-05-05 00:56:28 +0200

    cambios

--- ft_calloc.c
+++ ft_calloc.c
@@ -2,14 +2,14 @@
 
 void *ft_calloc(size_t number, size_t size)
 {
-    void    *ptr;
+	void	*ptr;
 
-    ptr = malloc(number * size);
-    if (!ptr)
+	ptr = malloc(number * size);
+	if (!ptr)
 	{
 		errno = ENOMEM;
 		return (NULL);
 	}
-    ft_memset(ptr, 0, number * size);
-    return (ptr);
+	ft_memset(ptr, 0, number * size);
+	return (ptr);
 }
