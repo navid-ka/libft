4caa20e2f78dc2ef4727876963ef5d86be3ae608
Your Name <you@example.com>
2023-05-04 18:09:37 +0200

    strlcpy paco aproved

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:30:18 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/04 13:25:39 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/05/04 18:08:56 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,22 @@
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
 {
-	size_t	pos;
-	size_t	len_src;
+	size_t	len;
+	size_t	i;
+	char	*ptrsrc;
+	char	*ptrdest;
 
-	pos = 0;
-	len_src = 0;
-	while (*(src + len_src))
-		++len_src;
-	if (src[pos] != 0)
+	ptrsrc = (char *)src;
+	ptrdest = (char *)dest;
+	len = ft_strlen(ptrsrc);
+	if (dstsize < 1)
+		return (len);
+	i = 0;
+	while (ptrsrc[i] && i < dstsize -1)
 	{
-		while (pos < dstsize -1 && *src)
-		{
-			dest[pos] = src[pos];
-			pos++;
-		}
-		if (!dest[pos])
-		{
-			dest[pos] = '\0';
-		}
+			ptrdest[i] = ptrsrc[i];
+			i++;
 	}
-	return (len_src);
+	ptrdest[i] = '\0';
+	return (len);
 }
