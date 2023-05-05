71f51e5b99d9e94191acd34cce2602fe7c97e21e
Your Name <you@example.com>
2023-05-05 19:26:14 +0200

    cositass ya cansado

--- ft_strtim.c
+++ ft_strtim.c
@@ -1,36 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strtim.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/05 15:06:25 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/05 18:30:29 by nkeyani-         ###   ########.fr       */
+/*   Created: 2023/05/05 18:53:53 by nkeyani-          #+#    #+#             */
+/*   Updated: 2023/05/05 18:53:56 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	len_s1;
-	size_t	len_s2;
+	size_t	len_set;
 	char	*res;
-	char	*ptrs1;
+	char	*ptrset;
 
-	if (!(ft_strlen(s1)) && !(ft_strlen(s2)))
-		return (ft_strdup(""));
-	ptrs1 = (char *)s1;
 	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	res = (char *) malloc(sizeof(char) * ((len_s1 + len_s2) + 1));
-	if (res == NULL)
+	len_set = ft_strlen(set);
+	ptrset = (char *)s1;
+	res = (char *) malloc(sizeof(char) * ((len_s1 + len_set) + 1));
 	{
 		errno = ENOMEM;
 		return (NULL);
 	}
-	ft_memmove(res, ptrs1, len_s1 + 1);
-	ft_strlcat(res, s2, (len_s1 + len_s2) + 1);
-	return (res);
 }
