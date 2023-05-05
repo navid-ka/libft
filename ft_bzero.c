c378412141538366c2fd49a15700096415389466
Bifrost <nkeyani-@student.42barcelona.com>
2023-05-05 12:02:01 +0200

    changed bzero to use memset

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 11:42:01 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/03 12:08:58 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/05/05 11:48:32 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+/*void	ft_bzero(void *s, size_t n)
 {
 	unsigned char	*ptrs;
 	size_t			i;
@@ -21,4 +21,9 @@ void	ft_bzero(void *s, size_t n)
 	i = 0;
 	while (i < n)
 		ptrs[i++] = 0;
+}*/
+
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
 }
