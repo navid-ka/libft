ba455aad7041bb7d8709b4e2cc501e8d60683e2d
Your Name <you@example.com>
2023-05-05 11:21:20 +0200

    normie

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/05 11:17:49 by nkeyani-          #+#    #+#             */
+/*   Updated: 2023/05/05 11:17:52 by nkeyani-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void *ft_calloc(size_t number, size_t size)
+void	*ft_calloc(size_t number, size_t size)
 {
 	void	*ptr;
 
