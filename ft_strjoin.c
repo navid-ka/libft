f4cc0ebabf90602dc07f80e17c7a78c9a283d023
Your Name <you@example.com>
2023-05-05 16:32:32 +0200

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,36 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/14 11:20:59 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/05 15:12:55 by nkeyani-         ###   ########.fr       */
+/*   Created: 2023/05/05 15:06:25 by nkeyani-          #+#    #+#             */
+/*   Updated: 2023/05/05 15:57:36 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	len;
-	int		i;
-	char	*dup;
+	size_t	len_s1;
+	size_t	len_s2;
+	char	*res;
+	char	*ptrs1;
 
-	len = ft_strlen(s);
-	dup = (char *) malloc(sizeof(char) * (len + 1));
-	if (dup == NULL)
+	if (!(ft_strlen(s1)) && !(ft_strlen(s2)))
+		return (ft_strdup(""));
+	ptrs1 = (char *)s1;
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	res = (char *) malloc(sizeof(char) * ((len_s1 + len_s2) + 1));
+	if (res == NULL)
 	{
 		errno = ENOMEM;
 		return (NULL);
 	}
-	i = 0;
-	while (s[i])
-	{
-		dup[i] = s[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
+	ft_memmove(res, ptrs1, ft_strlen(ptrs1) + 1);
+	ft_strlcat(res, s2, (len_s1 + len_s2) + 1);
+	return (res);
 }
