b6ca6c2b003d1eaa95c3f4b812afffef4d0668fe
Tu Nombre <you@example.com>
2023-05-11 11:47:06 +0200

    lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,10 +6,20 @@
 /*   By: bifrost <nkeyani-@student.42barcelona.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 02:57:43 by bifrost           #+#    #+#             */
-/*   Updated: 2023/05/11 03:02:07 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/05/11 11:37:08 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list 	*ft_lstnew(void *content);
+t_list 	*ft_lstnew(void *content)
+{
+	t_list  *node;
+	
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
