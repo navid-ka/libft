a80c07a245626c5c019c4dabd871eec35745cfcc
Tu Nombre <you@example.com>
2023-05-12 07:37:22 +0200

    lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <nkeyani-@student.42barcelona.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 23:26:34 by bifrost           #+#    #+#             */
-/*   Updated: 2023/05/12 00:00:29 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/05/12 07:36:49 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	ft_lstlast(*lst);
-	*lst = new->content;
+	t_list	*node;
 
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	node = ft_lstlast(*lst);
+	node->next = new;
 }
