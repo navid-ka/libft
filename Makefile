fbfa68490a5782a5d0c5dcde765302bf6194bb59
Your Name <you@example.com>
2023-05-15 17:07:00 +0200

    lst

--- Makefile
+++ Makefile
@@ -13,10 +13,11 @@ OBJECTS = ft_strlen.o ft_strncmp.o ft_isalpha.o \
 	ft_strjoin.o ft_strtrim.o ft_split.o \
 	ft_itoa.o ft_strmapi.o ft_striteri.o \
 	ft_putchar_fd.o ft_putstr_fd.o ft_putendl_fd.o \
-	ft_putnbr_fd.o
+	ft_putnbr_fd.o 
 
 OBJECTS_BONUS = ft_lstnew.o ft_lstadd_front.o ft_lstsize.o \
-	ft_lstlast.o ft_lstadd_back.o ft_lstdelone.o
+	ft_lstlast.o ft_lstadd_back.o ft_lstdelone.o \
+	ft_lstclear.o t_lstiter.o
 
 %.o : %.c
 	@printf "\rCompiling (╮°-°)╮┳━┳ : $<"
--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,22 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/15 13:26:16 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/15 15:26:16 by nkeyani-         ###   ########.fr       */
+/*   Created: 2023/05/15 15:27:54 by nkeyani-          #+#    #+#             */
+/*   Updated: 2023/05/15 16:02:51 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list	*node;
+	t_list	*middleman;
+	t_list	**node;
 
 	node = lst;
-	del(node->content);
-	free(node);
+	while (*node)
+	{
+		middleman = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = middleman;
+	}
 }
+
