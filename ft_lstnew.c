be432d1fa95450c7ab911907701f3260656e2799
Bifrost <nkeyani-@student.42barcelona.com>
2023-05-15 13:20:56 +0200

    normie

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bifrost <nkeyani-@student.42barcelona.c    +#+  +:+       +#+        */
+/*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 02:57:43 by bifrost           #+#    #+#             */
-/*   Updated: 2023/05/11 11:37:08 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/05/15 12:43:09 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list 	*ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-	t_list  *node;
-	
+	t_list	*node;
+
 	node = malloc(sizeof(t_list));
 	if (!node)
 		return (NULL);
