a08c6267b8a889b86d64cd46a4fdf5b19a36104e
Tu Nombre <nkeyani-@student.42barcelona.com>
2023-07-15 23:02:52 +0200

    split reset

--- src/strings/ft_split.c
+++ src/strings/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <nkeyani-@student.42barcelona.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 10:32:48 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/07/15 23:00:51 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/07/15 23:02:31 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,31 +37,12 @@ static size_t	ft_splitlen(const char *s, char c)
 	return (i);
 }
 
-/*static char	**is_present(char const *s, char c)
-{
-	char	**arr;
-
-	if (!s || !c)
-		return (NULL);
-	if (!ft_strchr(s, c))
-	{
-		arr = malloc(2 * sizeof(char *));
-		if (!arr)
-			return (NULL);
-		arr[0] = ft_strdup(s);
-		arr[1] = NULL;
-		return (arr);
-	}
-	return (NULL);
-}*/
-
 char	**ft_split(char const *s, char c)
 {
 	char	**arr;
 	char	*start;
 	size_t	i;
 
-	//is_present(s, c);
 	arr = ft_calloc((ft_splitlen(s, c) + 1), sizeof(char *));
 	if (!arr)
 		return (NULL);
@@ -75,9 +56,10 @@ char	**ft_split(char const *s, char c)
 			start = (char *)s;
 			while (*s && *s != c)
 				s++;
-			arr[i++] = ft_substr(start, 0, s - start);
+			arr[i] = ft_substr(start, 0, s - start);
 			if (!arr[i])
 				return (ft_freeall(arr, i));
+			i++;
 		}
 	}
 	arr[i] = NULL;
